6fcc3549415fcd8179452778c43851721ec1c471
aaronroe <aaronalkan@gmail.com>
2013-12-23 18:34:00 -0600

    Adds some basic routes for Play-Authenticate

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,16 @@ import views.html.*;
 
 public class Application extends Controller {
 
+    public static final String FLASH_ERROR_KEY = "error";
+
     public static Result index() {
         return ok(index.render());
+    }
+
+    public static Result oAuthDenied(final String providerKey) {
+        flash(FLASH_ERROR_KEY,
+                "You need to accept the OAuth connection in order to use this website!");
+        return redirect(routes.Application.index());
     }
 
 }
