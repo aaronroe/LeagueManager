f97ce72b4778e1ecda53027737e569f61ccf93c3
aaronroe <aaronalkan@gmail.com>
2013-12-27 23:56:51 -0600

    Adds league manager app name

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object Build extends sbt.Build {
 
-    val appName         = "play-authenticate-usage"
+    val appName         = "league-manager"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
@@ -17,21 +17,6 @@ object Build extends sbt.Build {
       javaEbean
     )
 
-//  Uncomment this for local development of the Play Authenticate core:
-/*
-    val playAuthenticate = play.Project(
-      "play-authenticate", "1.0-SNAPSHOT", Seq(javaCore, cache), path = file("modules/play-authenticate")
-    ).settings(
-      libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.2.5",
-      libraryDependencies += "com.feth" %% "play-easymail" % "0.5-SNAPSHOT",
-      libraryDependencies += "org.mindrot" % "jbcrypt" % "0.3m",
-      libraryDependencies += "commons-lang" % "commons-lang" % "2.6",
-
-      resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
-      resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns)
-    )
-*/
-
     val main = play.Project(appName, appVersion, appDependencies).settings(
 
       resolvers += Resolver.url("Objectify Play Repository (release)", url("http://schaloner.github.com/releases/"))(Resolver.ivyStylePatterns),
@@ -43,7 +28,5 @@ object Build extends sbt.Build {
       resolvers += Resolver.url("play-authenticate (release)", url("http://joscha.github.com/play-authenticate/repo/releases/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("play-authenticate (snapshot)", url("http://joscha.github.com/play-authenticate/repo/snapshots/"))(Resolver.ivyStylePatterns)
     )
-//  Uncomment this for local development of the Play Authenticate core:
-//    .dependsOn(playAuthenticate).aggregate(playAuthenticate)
-
+  
 }
