1f34966c2257ca9b2e85432ff4f09e04b878c403
aaronroe <aaronalkan@gmail.com>
2013-12-29 20:53:23 -0600

    Adds adhoc validation for initial roster size

--- app/models/forms/InitialRoster.java
+++ app/models/forms/InitialRoster.java
@@ -14,4 +14,16 @@ public class InitialRoster {
      */
     @Constraints.Required
     public List<Long> athletes;
+
+    /**
+     * Other validation for the initial roster.
+     * @return null, if there is no error, and a message otherwise.
+     */
+    public String validate() {
+        if (athletes.size() != 5) {
+            return "You must pick exactly 5 players for your intial roster.";
+        }
+
+        return null;
+    }
 }
