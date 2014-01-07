0d36020778e4b37125623a303d88b8dafd55e13e
aaronroe <aaronalkan@gmail.com>
2014-01-07 14:05:38 -0600

    Adds selection of athlete for roster init

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -2,7 +2,20 @@ var lmApp = angular.module('lmApp', []);
 
 lmApp.controller('RosterInitCtrl', function($scope, $http) {
 
+    $scope.selectedAthlete = null;
+
+    $scope.setSelectedAthlete = function(selectedAthlete) {
+        if ($scope.selectedAthlete != null) {
+            $scope.selectedAthlete["selected"] = false;
+        }
+        selectedAthlete["selected"] = true;
+        $scope.selectedAthlete = selectedAthlete;
+    };
+
     $http.get('/athlete/recruitable.json').then(function(result) {
+        for (var i=0; i < result.data.length; i++) {
+            result.data[i]["selected"] = false;
+        }
         $scope.recruitableAthletes = result.data;
     });
 
