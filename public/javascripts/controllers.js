9e8334b7e186e9ee0d82514dedab3cdfff89e081
aaronroe <aaronalkan@gmail.com>
2014-01-08 18:04:44 -0600

    Fixes code for selecting multiple athletes in roster init

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -2,19 +2,17 @@ var lmApp = angular.module('lmApp', []);
 
 lmApp.controller('RosterInitCtrl', function($scope, $http) {
 
-    $scope.selectedAthlete = null;
-
-    $scope.setSelectedAthlete = function(selectedAthlete) {
-        if ($scope.selectedAthlete != null) {
-            $scope.selectedAthlete["selected"] = false;
+    $scope.toggleAthleteSelected = function(athlete) {
+        if (athlete.selected == false) {
+            athlete.selected = true;
+        } else {
+            athlete.selected = false;
         }
-        selectedAthlete["selected"] = true;
-        $scope.selectedAthlete = selectedAthlete;
     };
 
     $http.get('/athlete/recruitable.json').then(function(result) {
         for (var i=0; i < result.data.length; i++) {
-            result.data[i]["selected"] = false;
+            result.data[i].selected = false;
         }
         $scope.recruitableAthletes = result.data;
     });
