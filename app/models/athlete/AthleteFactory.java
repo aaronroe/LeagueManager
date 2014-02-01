3f8836acd21521a145735b823b5dfa99b8622814
aaronroe <aaronalkan@gmail.com>
2014-01-31 23:40:50 -0600

    Makes player names a little more exciting, by adding more keywords

--- app/models/athlete/AthleteFactory.java
+++ app/models/athlete/AthleteFactory.java
@@ -34,12 +34,48 @@ public class AthleteFactory {
         words[11] = "Lord";
         words[12] = "Zephyr";
         words[13] = "Fuzzy";
+        words[14] = "Big";
+        words[15] = "x";
+        words[16] = "Cool";
+        words[17] = "Demon";
+        words[18] = "Angel";
+        words[19] = "Sheep";
+        words[20] = "The";
+        words[21] = "Best";
+        words[22] = "Crushing";
+        words[23] = "Baron";
+        words[24] = "Assassin";
+        words[25] = "Magi";
+        words[26] = "Mage";
+        words[27] = "Champion";
+        words[28] = "Thief";
+        words[29] = "Gosu";
+        words[30] = "Saint";
+        words[31] = "Vicious";
+        words[32] = "Dragon";
+        words[33] = "King";
+        words[34] = "Rock";
+        words[35] = "Shy";
+        words[36] = "Monk";
+        words[37] = "Mountain";
+        words[38] = "Insanity";
+        words[39] = "Scarred";
+        words[40] = "Pie";
+        words[41] = "Kiwi";
+        words[42] = "Kid";
+        words[43] = "Qt";
+        words[44] = "Muffin";
+        words[45] = "Chau";
+        words[46] = "Ambition";
+        words[47] = "Captain";
+        words[48] = "Fake";
+        words[49] = "Impact";
     }
 
     /**
      * Constant for the number of words in the dictionary.
      */
-    private final static int NUM_WORDS = 14;
+    private final static int NUM_WORDS = 50;
 
     /**
      * Array containing modifiers for use in an athlete name.
